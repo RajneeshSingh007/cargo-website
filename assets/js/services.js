7c660c5c94d56026948394102cee6e6878e4ce19
Rajneesh Singh <Rajneesh.Singh@syngenta.com>
2024-06-19 13:55:55 +0530

    initial commit

---
+++ assets/js/services.js
@@ -0,0 +1,161 @@
+const ServiceList = [
+  {
+    name: "Warehousing",
+    desc: `Our global footprint and extensive network means we have facilities at the source or destination ready to manage your supply chain. By combining transportation with warehousing and distribution, you benefit from a seamless end-to-end solution with fewer logistics service providers. Complexity is reduced and you gain control, visibility and speed.
+    `,
+    subheading: `Logistics Services We Offer in  Warehousing`,
+    info: [
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Complete and partial service</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Project uploads</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Transport of out of gauge goods</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Direct service from Turkey</p>
+      </div></div>`,
+    ],
+    img: "assets/images/services/04/4.jpg",
+  },
+  {
+    name: "Air Freight",
+    desc: `We know the importance of just-in-time shipment in terms of business efficiency and prestige of our valued customers, and we make our expertise in air transportation, as in other transportation areas.
+
+    As Bestcaucargo Logistics company, we ensure that our customers receive air transportation service, which is the fastest means of transportation and useful for long distances, at advantageous prices. Thanks to our contracted airports all over the world, we continue to produce fast solutions for your needs.
+    `,
+    subheading: `Logistics Services We Offer in Air Freight`,
+    info: [
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Complete and partial service</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Project uploads</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Transport of out of gauge goods</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Direct service from Turkey</p>
+      </div></div>`,
+    ],
+    img: "assets/images/services/04/1.jpg",
+  },
+  {
+    name: "Land Transport",
+    desc: `Intermodal transportation is the transportation of a load from one place to another with different modes of transportation without any change or operation on it. Thanks to the special trailer used in this transportation system, there is no need to remove the products from the trailer during vehicle change while being transported from one place to another. Many advantages and benefits of this transport system can be mentioned. As in every type of transportation, the important thing in this transportation system is to work with a successful and good company in this field. As Bestcaucargo, we always offer reliable transportation services to our valued customers with our intermodal transportation services.
+
+    `,
+    subheading: `What is Intermodal Transport?`,
+    info: [
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Thanks to the new systems developed in the field of transportation, things have started to be carried out more easily, especially in transportation from one country to another or overseas. This type of transportation refers to the transportation of an item or product from one country to another using different modes of transportation. During transportation, loads are also moved from one point to another. Thanks to the intermodal transportation services offered, people's work becomes easier when different vehicles are required to transport a product.
+      </p>
+      </div></div>`,
+    ],
+    img: "assets/images/services/04/2.jpg",
+  },
+  {
+    name: "Sea Freight",
+    desc: `Maritime transport is a method of transporting large quantities of various goods using ships. Goods packed in containers are then loaded onto a ship. A cargo ship has a carrying capacity of approximately 18 thousand containers. Maritime transport is a cost-effective way of transporting large volumes of cargo over long distances.
+
+    As Bestcaucargo, we safely transport your goods by sea at affordable prices.
+    
+    `,
+    subheading: `Logistics Services We Offer in Sea Freight`,
+    info: [
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Complete and partial service</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Project uploads</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Transport of out of gauge goods</p>
+      </div></div>`,
+      `<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12"><div class="services-details-check-mark d-flex  mb-50">
+      <span><i class="fa-sharp fa-regular fa-check"></i></span>
+      <p>Direct service from Turkey</p>
+      </div></div>`,
+    ],
+    img: "assets/images/services/04/3.jpg",
+  },
+];
+
+const Whatsapp_API = "https://wa.me/+905359638839?text=hi";
+const Instagram_API = "https://instagram.com/beshtau_kargo?igshid=YmMyMTA2M2Y=";
+
+$(document).ready(function () {
+  if (window.location.href.includes("services-details")) {
+    const idData = window.location.href.split("id");
+    if (idData.length === 2) {
+      const id = idData[1].replace(/=/g, "");
+      const position = id - 1;
+      $("#serviceImage").attr("src", ServiceList[Number(position)].img);
+      $("#serviceName").text(ServiceList[Number(position)].name);
+      $("#serviceSubHeading").text(ServiceList[Number(position)].subheading);
+      $("#serviceDesc").html(ServiceList[Number(position)].desc);
+      $("#serviceList").html(ServiceList[Number(position)].info.join("\n"));
+    } else {
+      window.location.href = "404.html";
+    }
+  }
+
+  $("#body").append(`<div class="whatsappContainer">
+  <div class="col">
+    <a target="_blank" class="instagramUrl">
+      <img src="assets/images/instagram.png" width="30" height="30"/>
+    </a>
+    <a target="_blank" class="whatsappUrl" style="margin-top:4px;">
+      <img src="assets/images/whatsapp.png" width="30" height="30"/>
+    </a>
+  </div>
+  </div>`);
+
+  $(".whatsappUrl").on("click", function () {
+    window.location.href = Whatsapp_API;
+  });
+  $(".instagramUrl").on("click", function () {
+    window.location.href = Instagram_API;
+  });
+
+  const lang = localStorage.getItem("lang");
+
+  if (lang !== undefined && lang !== null && lang.length > 0) {
+    const pos = countryList.findIndex(
+      (country) => country.toLowerCase() === lang.toLowerCase()
+    );
+    if (pos > -1) {
+      afterTranslate(pos);
+    }
+  } else {
+    russianTranslation("");
+  }
+
+  $('#langDiv').click(function(e) {  
+    $('.exampleModal').addClass('exampleModalVisible');
+  });
+
+  $('#langDiv1').click(function(e) {  
+    $('.exampleModal').addClass('exampleModalVisible');
+  });
+
+  $('.closeDiv').click(function(e) {  
+    $('.exampleModal').removeClass('exampleModalVisible');
+  });
+
+});
